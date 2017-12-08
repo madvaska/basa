a93881af30e1a84a233a39c4792f3eae4876aa2a
madvaska <netak.su@gmail.com>
2017-12-09 00:43:08 +0700

    4

--- partners/models.py
+++ partners/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Partner(models.Model):
+    INN = models.BigIntegerField()
+    email = models.EmailField()
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+    
