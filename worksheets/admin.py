3e4a26be72e857166a7d257598d2fdb2df5d6434
madvaska <netak.su@gmail.com>
2017-12-11 09:38:33 +0700

    7

--- worksheets/admin.py
+++ worksheets/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from worksheets.models import Worksheet
+from worksheets.models import Worksheet, WorksheetFile
 
 # Register your models here.
 admin.site.register(Worksheet)
+admin.site.register(WorksheetFile)
--- worksheets/models.py
+++ worksheets/models.py
@@ -4,16 +4,16 @@ from partners.models import Partner
 from datetime import datetime
 
 # Create your models here.
+class WorksheetFile(models.Model):
+    original = models.FileField(upload_to='uploads')
+    fileName = models.CharField(max_length=255, unique = True)
+
 class Worksheet(models.Model):
     partner = models.ForeignKey(Partner)
     typeOfWork = models.ForeignKey(TypeOfWork)
     importdatetime = models.DateTimeField(blank=True, default=datetime.now)
     comment = models.TextField(blank=True)
-    originalFile = models.ForeignKey(WorksheetFile)
+    originalFile = models.ForeignKey(WorksheetFile, blank = True, default = None)
 
     def __str__(self):
         return self.partner.name+self.typeOfWork.name
-
-class WorksheetFile(models.Model):
-    original = models.FileField(upload_to='uploads')
-    fileName = models.CharField(max_length=255, unique = True)
